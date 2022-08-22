b5356fd05805995a6b493069dbb6c9eebe19efe6
MSI <mohammarzy@gmail.com>
2022-08-22 23:10:54 +0200

    cloned in msi

--- app/src/main/java/com/pomonext/pomonext/navigation/PomoNavigation.kt
+++ app/src/main/java/com/pomonext/pomonext/navigation/PomoNavigation.kt
@@ -18,6 +18,7 @@ fun PomoNavigation() {
     val isUserLoggedIn = !(FirebaseAuth.getInstance().currentUser?.email.isNullOrEmpty())
 
 
+
     NavHost(
         navController = navController,
 //        startDestination = if (isUserLoggedIn) PomoScreens.HomeScreen.name else PomoScreens.LoginScreen.name
